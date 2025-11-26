5fa7a88eb70b2ba4727d3e1cebc5f933a84ec333
daily-learn32 <119475149+daily-learn32@users.noreply.github.com>
2025-11-26 10:15:17 +0700

    frontMent/14 (DONE)

---
+++ 02_FrontMent/js/script_14.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // Elemen-elemen untuk validasi email di footer
+  const footerForm = document.querySelector(".footer__absolute-input");
+  const emailInput = document.querySelector(
+    ".footer__absolute-input__frame__input"
+  );
+  const submitBtn = document.querySelector(".footer__absolute-input__button");
+  const messageEl = document.querySelector(
+    ".footer__absolute-input__frame__message"
+  );
+
+  // Fungsi remove classI
+  function removeClass() {
+    footerForm.classList.remove("error");
+    footerForm.classList.remove("correct");
+  }
+
+  // Fungsi untuk memvalidasi format email
+  function validateEmail(email) {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+  }
+
+  // Event listener untuk tombol submit
+  submitBtn.addEventListener("click", function () {
+    const email = emailInput.value;
+
+    // Hapus kelas status sebelumnya
+    footerForm.classList.remove("correct");
+    footerForm.classList.remove("error");
+
+    if (!validateEmail(email)) {
+      footerForm.classList.add("error");
+      messageEl.textContent = "Please enter a valid email address!";
+    } else {
+      footerForm.classList.add("correct");
+      messageEl.textContent = "Your registration was successful.";
+    }
+  });
+
+  // Event listener untuk input email (saat pengguna mengetik)
+  emailInput.addEventListener("input", () => {
+    removeClass();
+  });
+
+  // Penggilan awal
+  removeClass();
+});

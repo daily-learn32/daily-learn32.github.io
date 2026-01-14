7a21265b3bd8dfc1ee005b857a9fe89c9ef73bca
daily_learn32 <119475149+daily-learn32@users.noreply.github.com>
2026-01-14 21:43:58 +0700

    w3Schools/02 (DONE)

---
+++ 11_W3Schools/02/src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<script>
+  // FontAwesome
+  import "@fortawesome/fontawesome-free/css/all.min.css";
+
+  let { children } = $props();
+</script>
+
+<svelte:head>
+  <title>02 W3Schools HowTo</title>
+  <link rel="icon" href="./images/w3schools_logo.png" />
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Ubuntu+Sans:ital,wght@0,100..800;1,100..800&display=swap"
+    rel="stylesheet"
+  />
+</svelte:head>
+
+{@render children()}
+
+<style lang="scss">
+  :global(*) {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: "Ubuntu Sans", sans-serif;
+  }
+</style>
